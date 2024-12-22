179414771a3d3b598270dc06eed11774189cd355
Joao <joao.ham2001@gmail.com>
2024-12-21 23:35:10 -0300

    cansei e vou viajar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { motion } from 'framer-motion';
+
+import image1  from '../src/images/img1.jpg';
+import image2  from '../src/images/img2.jpg';
+import image3  from '../src/images/img3.jpg';
+import image4  from '../src/images/img4.jpg';
+
+let images = [image1, image2, image3, image4  ]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    <motion.div className="carousel" whileTap={{ cursor: "grabbing" }} >
+      <motion.div className="inner"
+      drag="x"
+      >
+
+        {images.map( image =>(
+          <motion.div className='item' key={image}>
+            <img src={image} alt="Texto alt" /> 
+          </motion.div>
+        ))}
+
+      </motion.div>
+    </motion.div>
+
+
     </div>
   );
 }
